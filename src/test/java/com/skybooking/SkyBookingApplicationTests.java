c9de65b1a381f4f960fd679520f409333b0858a0
Abdelrahman Eltokhy <abdelrahman.m.eltokhy@gmail.com>
2025-09-25 23:43:02 +0300

    First Commit 🚀

---
+++ src/test/java/com/skybooking/SkyBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skybooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
