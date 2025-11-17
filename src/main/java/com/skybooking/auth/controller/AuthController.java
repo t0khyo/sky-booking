49509bd72edc92052b81b290de9ac6210ce84711
Abdelrahman Eltokhy <abdelrahman.m.eltokhy@gmail.com>
2025-11-18 02:48:00 +0300

    feat: Add profile endpoint to AuthController and configure OAuth2 dependencies

--- src/main/java/com/skybooking/auth/controller/AuthController.java
+++ src/main/java/com/skybooking/auth/controller/AuthController.java
@@ -1,7 +1,19 @@
 package com.skybooking.auth.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/auth")
 public class AuthController {
+
+    @GetMapping("/profile")
+    public String getProfile(@AuthenticationPrincipal Jwt jwt) {
+        String givenName = jwt.getClaim("given_name");
+        String lastName = jwt.getClaim("family_name");
+        return "Hello, " + givenName + " " + lastName + "!";
+    }
 }
